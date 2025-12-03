60e5e4c6e3cbb20716a58da374b07e80b1d504a4
Danielasil <danielasilva081996@gmail.com>
2025-12-03 10:53:09 +0100

    favicon updated

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/weatherly-app/' 
+    : '/'  
+}
